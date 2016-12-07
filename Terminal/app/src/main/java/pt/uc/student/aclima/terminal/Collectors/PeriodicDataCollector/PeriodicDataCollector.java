881738ae74eb005fa0e93439e40c09c3115e1d7f
António Lima <tocalagarto93@gmail.com>
2016-12-07 01:37:58 +0000

    first tests with the IntentService class

--- Terminal/app/src/main/java/pt/uc/student/aclima/terminal/Collectors/PeriodicDataCollector/PeriodicDataCollector.java
+++ Terminal/app/src/main/java/pt/uc/student/aclima/terminal/Collectors/PeriodicDataCollector/PeriodicDataCollector.java
@@ -1,57 +1,62 @@
 package pt.uc.student.aclima.terminal.Collectors.PeriodicDataCollector;
 
 import android.app.IntentService;
-import android.content.Intent;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
  * <p>
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
+ * TODO: Customize class - update intent actions, extra parameters and static helper methods.
+ * 
+ * Collected Data:
+ * - RAM
+ * - CPU
+ * - GPS
+ * - CPU usage
+ * - RAM usage
+ * - Battery
+ * - Open Ports
+ * - Data Traffic
  */
 public class PeriodicDataCollector extends IntentService {
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_FOO = "pt.uc.student.aclima.terminal.Collectors.PeriodicDataCollector.action.FOO";
-    private static final String ACTION_BAZ = "pt.uc.student.aclima.terminal.Collectors.PeriodicDataCollector.action.BAZ";
+    private static final String ACTION_RAM = "pt.uc.student.aclima.terminal.Collectors.PeriodicDataCollector.action.RAM";
+    private static final String EXTRA_RAM_PARAM1 = "pt.uc.student.aclima.terminal.Collectors.PeriodicDataCollector.extra.PARAM1";
+    private static final String EXTRA_RAM_PARAM2 = "pt.uc.student.aclima.terminal.Collectors.PeriodicDataCollector.extra.PARAM2";
 
-    // TODO: Rename parameters
-    private static final String EXTRA_PARAM1 = "pt.uc.student.aclima.terminal.Collectors.PeriodicDataCollector.extra.PARAM1";
-    private static final String EXTRA_PARAM2 = "pt.uc.student.aclima.terminal.Collectors.PeriodicDataCollector.extra.PARAM2";
+    private static final String ACTION_CPU = "pt.uc.student.aclima.terminal.Collectors.PeriodicDataCollector.action.CPU";
 
     public PeriodicDataCollector() {
         super("PeriodicDataCollector");
     }
 
     /**
-     * Starts this service to perform action Foo with the given parameters. If
+     * Starts this service to perform action RAM with the given parameters. If
      * the service is already performing a task this action will be queued.
      *
      * @see IntentService
      */
     // TODO: Customize helper method
-    public static void startActionFoo(Context context, String param1, String param2) {
+    public static void startActionRAM(Context context, String param1, String param2) {
         Intent intent = new Intent(context, PeriodicDataCollector.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
+        intent.setAction(ACTION_RAM);
+        intent.putExtra(EXTRA_RAM_PARAM1, param1);
+        intent.putExtra(EXTRA_RAM_PARAM2, param2);
         context.startService(intent);
     }
 
     /**
-     * Starts this service to perform action Baz with the given parameters. If
+     * Starts this service to perform action CPU with the given parameters. If
      * the service is already performing a task this action will be queued.
      *
      * @see IntentService
      */
     // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
+    public static void startActionCPU(Context context) {
         Intent intent = new Intent(context, PeriodicDataCollector.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
+        intent.setAction(ACTION_CPU);
         context.startService(intent);
     }
 
@@ -59,32 +64,32 @@ public class PeriodicDataCollector extends IntentService {
     protected void onHandleIntent(Intent intent) {
         if (intent != null) {
             final String action = intent.getAction();
-            if (ACTION_FOO.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1, param2);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
+
+            if (ACTION_RAM.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_RAM_PARAM1);
+                final String param2 = intent.getStringExtra(EXTRA_RAM_PARAM2);
+                handleActionRAM(param1, param2);
+            }
+            else if (ACTION_CPU.equals(action)) {
+                handleActionCPU();
             }
         }
     }
 
     /**
-     * Handle action Foo in the provided background thread with the provided
+     * Handle action RAM in the provided background thread with the provided
      * parameters.
      */
-    private void handleActionFoo(String param1, String param2) {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
+    private void handleActionRAM(String param1, String param2) {
+        // TODO: Handle action RAM
+        Log.i( "RAM", param1 + " " + param2);
     }
 
     /**
-     * Handle action Baz in the provided background thread with the provided
+     * Handle action CPU in the provided background thread with the provided
      * parameters.
      */
-    private void handleActionBaz(String param1, String param2) {
+    private void handleActionCPU() {
         // TODO: Handle action Baz
         throw new UnsupportedOperationException("Not yet implemented");
     }
--- Terminal/app/src/main/java/pt/uc/student/aclima/terminal/CollectorsActivity.java
+++ Terminal/app/src/main/java/pt/uc/student/aclima/terminal/CollectorsActivity.java
@@ -1,7 +1,9 @@
 package pt.uc.student.aclima.terminal;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import pt.uc.student.aclima.terminal.Collectors.PeriodicDataCollector.PeriodicDataCollector;
 
 public class CollectorsActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class CollectorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collectors);
+
+        PeriodicDataCollector.startActionRAM(this, "foo", "bar");
+
     }
+
 }
