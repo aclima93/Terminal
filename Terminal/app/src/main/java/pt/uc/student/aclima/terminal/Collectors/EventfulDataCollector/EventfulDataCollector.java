47f26605826898e189620179efaf71e3f6176f60
António Lima <tocalagarto93@gmail.com>
2016-12-12 09:36:16 +0000

    added EventDataCollector BroadcastReceiver

---
+++ Terminal/app/src/main/java/pt/uc/student/aclima/terminal/Collectors/EventfulDataCollector/EventfulDataCollector.java
@@ -0,0 +1,17 @@
+package pt.uc.student.aclima.terminal.Collectors.EventfulDataCollector;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class EventfulDataCollector extends BroadcastReceiver {
+    public EventfulDataCollector() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
