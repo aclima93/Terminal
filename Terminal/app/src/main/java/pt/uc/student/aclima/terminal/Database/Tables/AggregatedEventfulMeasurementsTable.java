00c0986bf6671bf975e1f8c070c7625d8d0a1418
António Lima <tocalagarto93@gmail.com>
2016-12-13 10:44:32 +0000

    yet another class abstraction

--- Terminal/app/src/main/java/pt/uc/student/aclima/terminal/Database/Tables/AggregatedEventfulMeasurementsTable.java
+++ Terminal/app/src/main/java/pt/uc/student/aclima/terminal/Database/Tables/AggregatedEventfulMeasurementsTable.java
@@ -6,13 +6,11 @@ import pt.uc.student.aclima.terminal.Database.DatabaseManager;
  * Created by aclima on 13/12/2016.
  */
 
-public class AggregatedEventfulMeasurementsTable extends MeasurementsTable {
+public class AggregatedEventfulMeasurementsTable extends AggregatedMeasurementsTable {
 
     public static final String TABLE_NAME = "AggregatedEventfulMeasurementsTable";
 
     public static final String NUMBER_OF_EVENTS = "number_of_events";
-    public static final String SAMPLE_START_TIME = "sample_start_time";
-    public static final String SAMPLE_END_TIME = "sample_end_time";
 
     public AggregatedEventfulMeasurementsTable(DatabaseManager databaseManager) {
         super(databaseManager);
--- Terminal/app/src/main/java/pt/uc/student/aclima/terminal/Database/Tables/AggregatedPeriodicMeasurementsTable.java
+++ Terminal/app/src/main/java/pt/uc/student/aclima/terminal/Database/Tables/AggregatedPeriodicMeasurementsTable.java
@@ -6,15 +6,13 @@ import pt.uc.student.aclima.terminal.Database.DatabaseManager;
  * Created by aclima on 13/12/2016.
  */
 
-public class AggregatedPeriodicMeasurementsTable extends MeasurementsTable {
+public class AggregatedPeriodicMeasurementsTable extends AggregatedMeasurementsTable {
 
     public static final String TABLE_NAME = "AggregatedPeriodicMeasurementsTable";
 
     public static final String HARMONIC_VALUE = "harmonic_value";
     public static final String MEDIAN_VALUE = "median_value";
     public static final String UNITS_OF_MEASUREMENT = "units_of_measurement";
-    public static final String SAMPLE_START_TIME = "sample_start_time";
-    public static final String SAMPLE_END_TIME = "sample_end_time";
 
     public AggregatedPeriodicMeasurementsTable(DatabaseManager databaseManager) {
         super(databaseManager);
