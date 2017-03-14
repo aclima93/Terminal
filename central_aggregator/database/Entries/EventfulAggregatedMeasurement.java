dc43f7474175bca3d6f6ada78099a9f1e5803838
António Lima <tocalagarto93@gmail.com>
2017-03-14 10:50:29 +0000

    adding RDD classes for Spark DataFrames

---
+++ central_aggregator/database/Entries/EventfulAggregatedMeasurement.java
@@ -0,0 +1,25 @@
+package pt.uc.student.aclima.device_agent.Database.Entries;
+
+import java.util.Date;
+
+/**
+ * Created by aclima on 13/12/2016.
+ */
+
+public class EventfulAggregatedMeasurement extends AggregatedMeasurement {
+
+    private Integer numberOfEvents;
+
+    public EventfulAggregatedMeasurement(Integer id, String name, Date sampleStartTime, Date sampleEndTime, Integer numberOfEvents) {
+        super(id, name, sampleStartTime, sampleEndTime);
+        this.numberOfEvents = numberOfEvents;
+    }
+
+    public Integer getNumberOfEvents() {
+        return numberOfEvents;
+    }
+
+    public void setNumberOfEvents(Integer numberOfEvents) {
+        this.numberOfEvents = numberOfEvents;
+    }
+}
---
+++ central_aggregator/database/Entries/PeriodicAggregatedMeasurement.java
@@ -0,0 +1,45 @@
+package pt.uc.student.aclima.device_agent.Database.Entries;
+
+import java.util.Date;
+
+/**
+ * Created by aclima on 13/12/2016.
+ */
+
+public class PeriodicAggregatedMeasurement extends AggregatedMeasurement{
+
+    private Double harmonicValue;
+    private Double medianValue;
+    private String unitsOfMeasurement;
+
+    public PeriodicAggregatedMeasurement(Integer id, String name, Date sampleStartTime, Date sampleEndTime, Double harmonicValue, Double medianValue, String unitsOfMeasurement) {
+        super(id, name, sampleStartTime, sampleEndTime);
+        this.harmonicValue = harmonicValue;
+        this.medianValue = medianValue;
+        this.unitsOfMeasurement = unitsOfMeasurement;
+    }
+
+    public Double getHarmonicValue() {
+        return harmonicValue;
+    }
+
+    public void setHarmonicValue(Double harmonicValue) {
+        this.harmonicValue = harmonicValue;
+    }
+
+    public Double getMedianValue() {
+        return medianValue;
+    }
+
+    public void setMedianValue(Double medianValue) {
+        this.medianValue = medianValue;
+    }
+
+    public String getUnitsOfMeasurement() {
+        return unitsOfMeasurement;
+    }
+
+    public void setUnitsOfMeasurement(String unitsOfMeasurement) {
+        this.unitsOfMeasurement = unitsOfMeasurement;
+    }
+}
